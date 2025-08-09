ef5430d89812b970a4c84fd3f4f551439615fb10
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-09 15:18:08 +0530

    Add files via upload

---
+++ PracticeQ1.js
@@ -0,0 +1,9 @@
+// Q1 Write a poem using Functions:- 
+function myPoem () {
+    console.log("Life is something weve been blessed,");
+    console.log(" Choice is yours; choose your quest.");
+    console.log(" Follow your passions, and youll be fine");
+    console.log("With the right attitude, you will shine.");
+}
+
+myPoem();
