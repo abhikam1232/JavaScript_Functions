5719fd6cec8dcf6d9a0d466432e8aa74ac22223e
abhikam1232 <abhikam152002@gmail.com>
2025-08-09 15:06:42 +0530

    Basic introduction to Functions

---
+++ app.js
@@ -0,0 +1,42 @@
+function myName() {
+    console.log("My name is Abhishek kamble");
+    console.log("Currently I am Learning MERN stack Development");
+}
+
+myName();
+
+function sgpa_Skills() {
+    console.log("I FE my SGPA was:- 6");
+    console.log("In SE My SGPA was 8.05");
+    console.log(" In TE My Sgpa was around 8.50");
+    console.log(" Basic computer fundamentals, HTML, CSS, Bootstrap, Javascript, Using Ai tools ");
+}
+
+sgpa_Skills();
+
+
+
+function isAdult() {
+    let age = 18;
+    if ( age >= 18){
+        console.log("I am an Adult");
+    } else if (age >= 16 && age<=18){
+        console.log("I am a Teenager  ");
+    } else{
+        console.log(" I am a kid");
+    }
+}
+
+isAdult();
+
+function canMarry() {
+    let your_age = 23;
+    if (your_age >= 22){
+        console.log(" A new chapter opens in your life Congratulations you can marry");
+    } else {
+        console.log(" You can't Marry");
+    }
+}
+
+canMarry();
+
