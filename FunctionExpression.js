bc1f996e8e59f05e8c3a158bf63a2b53f4e05bdb
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-11 23:54:50 +0530

    Add files via upload
    Function Expresssion Another way to write the Function:-

---
+++ FunctionExpression.js
@@ -0,0 +1,13 @@
+// These are function expression another way to create a function:- 
+let value = function(a, b){
+    return a+b;
+}
+
+console.log(value(20, 50));
+
+let hello = function(){
+    console.log("Welcome");
+}
+
+
+console.log(hello());
