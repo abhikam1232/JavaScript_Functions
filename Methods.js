ef67763cc8be676ce3353f71f902db1c4a38ada5
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-13 01:46:35 +0530

    Add files via upload

---
+++ Methods.js
@@ -0,0 +1,18 @@
+const calculator = {
+
+    add: function(a, b){
+        return a+b;
+    },
+
+    sub: function(x, y){
+        return x-y;
+    }, 
+
+    mul: function(n,m){
+        return n*m;
+    }, 
+
+    divide: function(l,o){
+        return l/o;
+    }
+}
