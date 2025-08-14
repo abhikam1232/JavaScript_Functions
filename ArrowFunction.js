aabe7a045028c2f4f13e403db3f4a174450c488c
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-14 17:48:44 +0530

    Add files via upload
    Arrow functions:-

---
+++ ArrowFunction.js
@@ -0,0 +1,19 @@
+
+
+const sum = (a, b) => a+b; // This performs simple functions using Arrow; 
+console.log(sum(50, 50));
+
+// Calcute the cube using Arrow functions:- 
+
+const cube = (n) => {
+        return n*n*n;
+}
+
+console.log(cube(3));
+
+// Calculate the function a to the power b using Arrow functions:- 
+const power = (a,b) => {
+    return a**b;
+}
+
+console.log(power(9,2));
