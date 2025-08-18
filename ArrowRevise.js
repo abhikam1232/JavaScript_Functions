f59312c7e21ed5dc00e118aaed59a7cb63a04bfd
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-19 00:30:12 +0530

    Add files via upload

---
+++ ArrowRevise.js
@@ -0,0 +1,19 @@
+const sum = (a, b) => {
+    console.log(a+b);
+};
+
+const cube = (n) => {
+    return n*n*n;
+};
+
+const power = (x) => {
+    return x**x;
+};
+
+const EvenOdd = (i) => {
+    if ( i%2 == 0){
+        console.log(" It is even");
+    } else{
+        console.log("It is odd");
+    }
+}
---
+++ error.js
@@ -0,0 +1,7 @@
+let a = 50;
+console.log(a);
+try{
+    console.log(b);
+} catch{
+    console.log(" b variable is basically not defined ");
+}
