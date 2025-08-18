d76a9fdb4e46f2ecee610d7b8f37b83a3a523dc3
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-19 00:44:02 +0530

    Add files via upload

---
+++ implicitArrow.js
@@ -0,0 +1,8 @@
+const sum = (n, m) => (
+    n+m    // This is an implicit Arrow function where you don't need to return any statment in it it is implicit..
+);
+console.log(sum(5,4));
+
+// Another implicit Function..
+
+const cube = (i) => i*i*i;
