df2913ad43b1f768ccc4af1b2a065d66e9271f5a
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-21 01:28:53 +0530

    Add files via upload
    CountDown of numbers from 5 to 1 using setTimeout Function....

---
+++ setTimeout2.js
@@ -0,0 +1,25 @@
+console.log(" Starting Countdown");
+
+setTimeout ( () => {
+    console.log("5");
+}, 1000);
+
+setTimeout ( () => {
+    console.log("4");
+}, 2000);
+
+setTimeout ( () => {
+    console.log("3");
+}, 3000);
+
+setTimeout ( () => {
+    console.log("2");
+}, 4000);
+
+setTimeout ( () => {
+    console.log("1");
+}, 5000);
+
+
+
+console.log("GO !!!");
