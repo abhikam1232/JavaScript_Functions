8849f31bc8bc920457cc1463793d250d244235bd
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-21 01:33:35 +0530

    Add files via upload

---
+++ setTimeout3.js
@@ -0,0 +1,18 @@
+/**Make a program where:
+
+First, it prints "Hello!" immediately.
+
+After 2 seconds, it prints "How are you?"
+
+After 4 seconds, it prints "Goodbye!" */
+
+
+console.log(" Hello!");
+
+setTimeout( () => {
+    console.log(" How are you???");
+}, 2000);
+
+setTimeout( () => {
+    console.log(" GOODBYE !! ");
+}, 4000);
