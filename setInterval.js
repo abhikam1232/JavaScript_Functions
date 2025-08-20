bc943e1683695663b129450f45d10a372b49a8b4
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-21 01:53:35 +0530

    Add files via upload
    Basic Introduction to Set interval

---
+++ setInterval.js
@@ -0,0 +1,11 @@
+// It represents simple function of setInterval...
+
+let id = setInterval ( () => {
+    console.log(" Abhishek here ");
+}, 1000);
+console.log(id);
+
+let id2 = setInterval ( () => {
+    console.log(" Hello World");
+}, 2000);
+console.log(id2);
