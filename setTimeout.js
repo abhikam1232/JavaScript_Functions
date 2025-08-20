5e10f98e384dcc6d7632a285dc7add278ef6751a
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-21 01:13:01 +0530

    Add files via upload
    These are some basic SetTimeout functions....

---
+++ setTimeout.js
@@ -0,0 +1,9 @@
+// The main Function of set timeout is used for API calls, requests...
+
+console.log( " Hii there my name is Abhishek");
+
+setTimeout( () => {
+    console.log(" I am basically learning javascript");
+}, 2000);
+
+console.log(" I am learning this from Apna College Platform");
---
+++ setTimeout1.js
@@ -0,0 +1,12 @@
+// Q1. Create a program that prints your name immediately, then after 3 seconds it should print your favorite hobby. by using setTime out Function..
+
+
+console.log(" Name:- Abhishek Kamble");
+
+setTimeout ( () => {
+    console.log(" Hobby:- Coding, riding bikes etc");
+}, 3000);
+console.log("----------------------");
+console.log(" This is setTimeout Function:- ");
+console.log("------------------------");
+
