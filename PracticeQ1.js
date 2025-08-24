47906c60d40d1db99ebedb6ad454a95b2e44b88f
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-25 00:47:08 +0530

    Add files via upload
    Some problems using setTimeout and setInterval

--- PracticeQ1.js
+++ PracticeQ1.js
@@ -1,9 +1,43 @@
-// Q1 Write a poem using Functions:- 
-function myPoem () {
-    console.log("Life is something weve been blessed,");
-    console.log(" Choice is yours; choose your quest.");
-    console.log(" Follow your passions, and youll be fine");
-    console.log("With the right attitude, you will shine.");
-}
+// Write an Arrow Function that returns the Square of a number..
 
-myPoem();
+const mySquare = (n) => (n*n);
+
+const myCube = (x) => (x*x*x);
+
+//Q2. Write a function that prints " hello world" 5 times at intervals of 2 secs each...
+
+/** let id = setInterval ( () => {
+    console.log(" Hello What's going on!!!");
+}, 2000);
+
+setTimeout( () =>{
+    clearInterval(id);
+}, 10000); **/
+
+
+// Q3 Print hello Abhishek 5 times and than stop... 
+
+const myPrint = () => {
+
+    setTimeout( () => {
+        console.log(" Hello Abhishek");
+    }, 1000);
+
+    setTimeout( () => {
+        console.log(" Hello Abhishek");
+    }, 2000);
+    
+    setTimeout( () => {
+        console.log(" Hello Abhishek");
+    }, 3000);
+    
+    setTimeout( () => {
+        console.log(" Hello Abhishek");
+    }, 4000);
+
+    setTimeout( () => {
+        console.log(" Hello Abhishek");
+    }, 5000);
+    
+}
+console.log(myPrint());
